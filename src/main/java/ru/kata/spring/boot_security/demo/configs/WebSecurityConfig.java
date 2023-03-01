c8c99206d45b77828726d75097d2a8f38e605f9c
Minin81 <nikolai.minin81@gmail.com>
2023-03-01 10:57:41 +0300

    Commit (db pass fixed)

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/index", "/api/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-
+                .antMatchers("").hasAuthority("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().successHandler(successUserHandler)
